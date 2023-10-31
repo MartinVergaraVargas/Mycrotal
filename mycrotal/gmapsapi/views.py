90454db290f9cc961041c71cd27f43cc0c5e2a7d
MartinVergaraVargas <vergaravargas.martin@gmail.com>
2023-10-31 16:45:42 -0300

    se agrega template de mapa, url y view en gmapsapi

--- mycrotal/gmapsapi/views.py
+++ mycrotal/gmapsapi/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from django.views import View
+from django.shortcuts import render, redirect
+from .models import *
+import googlemaps
+from django.conf import settings
+from .forms import *
+from datetime import datetime
 
 # Create your views here.
+
+class MapView(View): 
+    template_name = "mapa.html"
+
+    def get(self,request): 
+        key = settings.GOOGLE_API_KEY
+        eligable_locations = Locations.objects.filter(place_id__isnull=False)
+        locations = []
+
+        for a in eligable_locations: 
+            data = {
+                'lat': float(a.lat), 
+                'lng': float(a.lng), 
+                'name': a.name
+            }
+
+            locations.append(data)
+
+
+        context = {
+            "key":key, 
+            "locations": locations
+        }
+
+        return render(request, self.template_name, context)
