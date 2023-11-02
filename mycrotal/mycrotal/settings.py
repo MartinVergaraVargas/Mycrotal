46ad2685646ff99500bb65ebd2a0a35cc5ed4c41
MartinVergaraVargas <vergaravargas.martin@gmail.com>
2023-11-02 01:33:59 -0300

    se agrega rama datos

--- mycrotal/mycrotal/settings.py
+++ mycrotal/mycrotal/settings.py
@@ -115,7 +115,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'static/'  # URL estática para archivos de estilos y JavaScript
+STATICFILES_DIRS = ['static']  # Rutas a directorios de archivos estáticos
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
