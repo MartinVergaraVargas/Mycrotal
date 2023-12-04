0effa828cf5f48707ca24ea6ed31cb99487b3d97
MartinVergaraVargas <vergaravargas.martin@gmail.com>
2023-12-04 02:12:06 -0300

    se avanza

--- mycrotal/datosMicros/models.py
+++ mycrotal/datosMicros/models.py
@@ -8,7 +8,7 @@ class Linea(models.Model):
 
 class Paradero(models.Model):
     nombre = models.CharField(max_length=100)
-    ubicacion = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=255)
 
     def __str__(self):
         return self.nombre
@@ -24,4 +24,30 @@ class Ruta(models.Model):
 """
 ruta = Ruta(linea=linea_instancia, orden=1, paradero=paradero_instancia)
 ruta.save()
+"""
+
+"""
+from django.db import models
+
+class Linea(models.Model):
+    nombre = models.CharField(max_length=100)
+    empresa = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Paradero(models.Model):
+    nombre = models.CharField(max_length=100)
+    ubicacion = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Ruta(models.Model):
+    linea = models.ForeignKey(Linea, on_delete=models.CASCADE)
+
+class Ruta_asignada(models.Model):
+    ruta = models.ForeignKey(Ruta, on_delete=models.CASCADE)
+    orden = models.PositiveIntegerField()
+    paradero = models.ForeignKey(Paradero, on_delete=models.CASCADE)
 """
--- mycrotal/gmapsapi/views.py
+++ mycrotal/gmapsapi/views.py
@@ -1,3 +1,16 @@
+# views.py
+import googlemaps
 from django.shortcuts import render
+from django.conf import settings
 
-# Create your views here.
+def mostrar_mapa(request):
+    context = {
+        'GOOGLE_API_KEY': settings.GOOGLE_API_KEY
+    }
+    return render(request, 'mostrarMapa.html', context)
+    #return render(request, 'gmapsapi/mostrarMapa.html', {'api_key': settings.GOOGLE_API_KEY})
+
+def calcular_ruta(request):
+    gmaps = googlemaps.Client(key=settings.GOOGLE_API_KEY)
+    directions_result = gmaps.directions('Paradero A', 'Paradero B', mode="transit")
+    return render(request, 'gmapsapi/calculadora_rutas.html', {'directions_result': directions_result})
