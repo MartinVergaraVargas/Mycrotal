9aabb5bfe074d0170d6baa3954c6cbd4de9b14f6
MartinVergaraVargas <vergaravargas.martin@gmail.com>
2023-12-17 19:42:10 -0300

    se integran los ultimos cambios

--- mycrotal/gmapsapi/views.py
+++ mycrotal/gmapsapi/views.py
@@ -2,13 +2,26 @@
 import googlemaps
 from django.shortcuts import render
 from django.conf import settings
+from datosMicros.models import Paradero
+from django.core.serializers import serialize
+from django.http import JsonResponse
 
-def mostrar_mapa(request):
+
+""" def mostrar_mapa(request):
     context = {
         'GOOGLE_API_KEY': settings.GOOGLE_API_KEY
     }
     return render(request, 'mostrarMapa.html', context)
     #return render(request, 'gmapsapi/mostrarMapa.html', {'api_key': settings.GOOGLE_API_KEY})
+ """
+def mostrar_mapa(request):
+    paraderos = serialize('json', Paradero.objects.all(), fields=('nombre', 'ubicacion'))
+    context = {
+        'GOOGLE_API_KEY': settings.GOOGLE_API_KEY,
+        'paraderos': paraderos,
+    }
+    return render(request, 'mostrarMapa.html', context)
+
 
 def calcular_ruta(request):
     gmaps = googlemaps.Client(key=settings.GOOGLE_API_KEY)
