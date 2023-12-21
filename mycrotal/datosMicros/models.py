a3efcacb07bf0e418524d5851f4e74908c997f69
FranciscoAmigoQuintana <38572149+FranciscoAmigoQuintana@users.noreply.github.com>
2023-12-21 12:10:47 -0300

    cambio base datos (clase) 07/12/23  models.py
    Se realizo el cambio en al base de datos para que tuviera concordancia con lo solicitado por el profesor en la presentación del 75%, en donde pidió el cambio de la relación entre la tabla ruta y paradero, en la cual la forenkey de ruta, que era paradero, pasa a estar en paradero como ruta(forenkey).

--- mycrotal/datosMicros/models.py
+++ mycrotal/datosMicros/models.py
@@ -24,6 +24,7 @@ class Micro(models.Model):
 class Paradero(models.Model):
     nombre = models.CharField(max_length=100)
     ubicacion = models.CharField(max_length=255)
+    ruta = models.ForeignKey(Paradero, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.nombre
@@ -31,7 +32,6 @@ class Paradero(models.Model):
 class Ruta(models.Model):
     linea = models.ForeignKey(Linea, on_delete=models.CASCADE)
     orden = models.PositiveIntegerField()
-    paradero = models.ForeignKey(Paradero, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'Ruta de {self.linea.nombre}'
